d1eafcdb5ddd4c1bd0ea7c122fd632f3c02b6582
artem.ermolaev <ermolaev123>
2021-07-12 17:16:03 +0300

    CBDCT-125 filter fixes

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        // target: 'https://cbdc.factory.bcgp.io/api',
-        target: 'http://localhost:8080',
+        target: 'https://cbdc.factory.bcgp.io/api',
+        // target: 'http://localhost:8080',
         pathRewrite: { '^/api': '' }
       },
       '/images': {
