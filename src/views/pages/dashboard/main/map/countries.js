c36ad2499438672fc4db7e3d3110786ed5a89702
Igor Struchkov <70892702+igor-struchkov@users.noreply.github.com>
2021-07-22 17:19:48 +0300

    Fixed Vietnam country name

--- src/views/pages/dashboard/main/map/countries.js
+++ src/views/pages/dashboard/main/map/countries.js
@@ -277,7 +277,7 @@ export const REAL_COUNTRIES = [
   { name: 'Uzbekistan', code: 'UZ' },
   { name: 'Vanuatu', code: 'VU' },
   { name: 'Venezuela', code: 'VE' },
-  { name: 'Viet Nam', code: 'VN' },
+  { name: 'Vietnam', code: 'VN' },
   { name: 'Virgin Islands (British)', code: 'VG' },
   { name: 'Virgin Islands (U.S.)', code: 'VI' },
   { name: 'Wallis and Futuna', code: 'WF' },
